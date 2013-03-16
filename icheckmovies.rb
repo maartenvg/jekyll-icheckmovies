90164713041f0e65ad72c9da5314dfdbe68ff335
maartenvg <git@maartenvangrootel.nl>
2013-03-16 08:41:39 -0400

    Add connection to The MovieDB via ruby-tmdb3

--- icheckmovies.rb
+++ icheckmovies.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'   
 require 'open-uri'  
 require 'ruby-tmdb3'
+require 'date'
 
 url = 'http://www.icheckmovies.com/movies/checked/?user=maartenvg'
 
@@ -13,12 +14,17 @@ Tmdb.default_language = "en"
 
 doc = Nokogiri::HTML(open(url))  
   
-doc.css('li.listItemMovie')[0,4].each do | movie | 
+doc.css('li.listItemMovie')[0,1].each do | movie | 
     name = movie.css('h2 a').first.content()
     imdb = movie.css('.optionIMDB').first.attribute('href').value()
     year = movie.css('.info a').first.content()
-    p name
-    p imdb
-    p year
+    
+    tmdb = TmdbMovie.find(:imdb => imdb[/tt[0-9]+/], :limit => 1, :expand_results => false)
+    tmdb_year = Date.parse(tmdb.release_date).year
+    
+    if name != tmdb.title || year != tmdb_year.to_s
+      puts "AARRGGHH"
+    end
+    post = tmdb.posters[0].sizes.w185.url
 end 
 
